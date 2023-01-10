7d704171a32b4d2790bb56c4f3f341ef2c304738
Tyler Camp <tylerc@synopsys.com>
2023-01-10 12:23:52 -0500

    Update license headers for 2023

--- src/main/java/com/secdec/codedx/security/CertificateAcceptance.java
+++ src/main/java/com/secdec/codedx/security/CertificateAcceptance.java
@@ -1,6 +1,5 @@
 /*
- *
- * © 2022 Synopsys, Inc. All rights reserved worldwide.
+ * © 2023 Synopsys, Inc. All rights reserved worldwide.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -12,9 +11,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License
- *
  */
-
 package com.secdec.codedx.security;
 
 /**
